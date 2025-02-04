45ca7a1dee062abbb6edc85dfcdc7ab44ebd337b
Pratyaya_roy <pratyaya@impactdash.com>
2025-02-04 11:05:13 +0530

    Initial commit

---
+++ demoPython.py
@@ -0,0 +1,7 @@
+import sys
+
+# Read parameters from command-line arguments
+version = sys.argv[1]  # First argument
+market = sys.argv[2]   # Second argument
+
+print(f"Deploying version {version} to {market} Market...")
